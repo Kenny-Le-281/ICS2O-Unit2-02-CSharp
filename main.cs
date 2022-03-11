a749cda3cdf30c91037e6093ac5d72d118380d7d
Kenny-Le-281 <99355017+Kenny-Le-281@users.noreply.github.com>
2022-03-11 18:22:48 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,19 @@
+// Created by: Kenny Le
+// Created on: Mar 2022
+//
+// This program calculates the perimeter and area
+
+using System; 
+
+public class Program 
+{
+
+    public static void Main(string[] args) 
+    {
+        Console.WriteLine("The dimensions of a rectangle are 5 cm and 3 cm.");
+        Console.WriteLine("The area: " + (5 * 3));
+        Console.WriteLine("The perimeter: " + (2 * (5 + 3)));
+
+        Console.WriteLine("\nDone.");
+    }
+}
